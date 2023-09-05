875030471a5beb4c35868f51fd25edc2b521f736
zyzik1248 <zuzannadudzinska96@gmail.com>
2023-09-05 13:18:09 +0200

    add download

--- src/components/DownloadItem.tsx
+++ src/components/DownloadItem.tsx
@@ -1,12 +1,13 @@
 import { MaterialQuery } from "@/types"
-import Image from "next/image"
-import Link from "next/link"
+import { toBase64 } from "@/utilis/convertToBase64"
 
-const DownloadItem: React.FC<MaterialQuery> = ({name, asset}) =>{
+const DownloadItem: React.FC<MaterialQuery> = async ({name, asset}) =>{
+    const image = await toBase64(asset.preview)
+
     return(
-        <div className="px-big mt-[100px] w-[190px] sm:w-[240px] flex items-center flex-col con">
-            <div className="relative h-[120px] shadow-lg mb-5 w-full sm:h-[180px]">
-                <Image alt="name" layout="fill" src={asset.preview}/>
+        <div className="mt-[100px] w-[300px] flex items-center flex-col con">
+            <div className="relative shadow-lg mb-5 w-full ">
+                <img className="w-full h-full object-contain" src={`data:image/png;base64, ${image}`} alt={name} />
             </div>
             <p className="text-center m-auto text-green text-sm sm:text-base" >{name}</p>
         </div>
