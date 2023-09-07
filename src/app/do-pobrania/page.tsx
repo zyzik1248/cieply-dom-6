8d1d790b0bc15f4ba90c88964e9f24ca5cffde06
zyzik1248 <zuzannadudzinska96@gmail.com>
2023-09-07 12:57:46 +0200

    clar cahce data

--- src/app/do-pobrania/page.tsx
+++ src/app/do-pobrania/page.tsx
@@ -11,7 +11,6 @@ async function getData() {
         headers: {
           "Content-Type": "application/json",
         },
-        next: { revalidate: false },
         cache: "no-store"
       }
     );
