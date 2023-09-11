b13ace34cfff8e60004c895e2e3681ddd5ff9b3f
zyzik1248 <zuzannadudzinska96@gmail.com>
2023-09-11 13:17:52 +0200

    add articles

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/uploads/:name', 
+        destination: `${process.env.NEXT_PUBLIC_STRAPI}/uploads/:name`,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
@@ -15,6 +23,12 @@ const nextConfig = {
         protocol: "https",
         hostname: "media.graphassets.com",
         port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
         pathname: "/**",
       },
     ],
--- src/types.ts
+++ src/types.ts
@@ -1,45 +1,65 @@
-export interface IndexQuery{
-    id: string
-    name: string
-    description: string
-    slug: string
-    content: string
-    longSlug: string
+export interface IndexQuery {
+  id: string;
+  name: string;
+  description: string;
+  slug: string;
+  content: string;
+  longSlug: string;
 }
 
-export interface MaterialQuery{
-    name: string
-    id: string
-    asset: {
-        id:string
-        url:string
-        preview:string
-    }
+export interface MaterialQuery {
+  name: string;
+  id: string;
+  asset: {
+    id: string;
+    url: string;
+    preview: string;
+  };
 }
 
-export interface NewsQuery{
-    id: string
-    title: string
-    date: string
-    postedDate: string
-    slug: string
-    description: {
-        html: string
-        text: string
-    }
-    content: {
-        html: string
-        text: string
-    }
-    imagePreview: {
-        id:string
-        url:string
-    }
+export interface NewsQuery {
+  id: string;
+  title: string;
+  date: string;
+  postedDate: string;
+  slug: string;
+  description: {
+    html: string;
+    text: string;
+  };
+  content: {
+    html: string;
+    text: string;
+  };
+  imagePreview: {
+    id: string;
+    url: string;
+  };
 }
 
-export interface MaterialPostQuery{
-    name: string
-    id: string
-    count: string
-    email: string
+export interface MaterialPostQuery {
+  name: string;
+  id: string;
+  count: string;
+  email: string;
+}
+
+export interface ArticleQuery {
+  id: string;
+  attributes: {
+    title: string;
+    date: string;
+    datePosted: string;
+    slug: string;
+    content: string;
+    description: string;
+    cover: {
+      data: {
+        id: string;
+        attributes: {
+          url: string;
+        };
+      };
+    };
+  };
 }
